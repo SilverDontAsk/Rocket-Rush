015bc4882fb2f3b3c5b1795554df11da9a3d7ca8
Silver Rosario <147434626+SilverDontAsk@users.noreply.github.com>
2024-10-15 15:13:03 -0400

    pushing some of mitzael's work. Still need to make it work fluidly and work on the obstacles

---
+++ main_character.gd
@@ -0,0 +1,52 @@
+extends Area2D
+
+@onready var animations : AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_up : RayCast2D = $Raycast/up
+@onready var ray_down : RayCast2D = $Raycast/down
+@onready var ray_left : RayCast2D = $Raycast/left
+@onready var ray_rigt : RayCast2D = $Raycast/right
+const PIXELS : int = 15
+var tween : Tween
+
+func _ready():
+	pass 
+
+func _process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if direction:
+		move_me(direction)
+		
+		
+func move_me(direction):
+	
+	var next_position : Vector2
+	
+	if direction.x < 0 && !ray_left.is_colliding():
+		next_position = position + Vector2(-PIXELS, 0)
+		animations.play("Spaceship_flying")
+		move_by_tween(next_position)
+		#left
+	elif direction.x > 0 && !ray_down.is_colliding():
+		next_position = position + Vector2(PIXELS, 0)
+		animations.play("Spaceship_flying")
+		move_by_tween(next_position)
+		#right
+	elif direction.y < 0 && !ray_up.is_colliding():
+		next_position = position + Vector2(0, -PIXELS)
+		animations.play("Spaceship_flying")
+		move_by_tween(next_position)
+		#up
+	elif direction.y > 0 && !ray_down.is_colliding():
+		next_position = position + Vector2(0, PIXELS)
+		animations.play("Spaceship_flying")
+		move_by_tween(next_position)
+		#down
+		
+func move_by_tween(next_position : Vector2):
+	tween = create_tween()
+	tween.tween_property(self, "position", next_position, 0.2)
+	tween.tween_callback(end_of_tween)
+	
+func end_of_tween():
+	animations.play("Spaceship_flying")
