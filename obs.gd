6f75505b02a0c10be9750d8ff23c0dc3237a87dc
Silver Rosario <147434626+SilverDontAsk@users.noreply.github.com>
2024-10-21 17:23:12 -0400

    fixed an obstacle crash for now but they wont spawn and added borders to the game scene

--- obs.gd
+++ obs.gd
@@ -8,12 +8,5 @@ func _ready() -> void:
 	$AnimatedSprite2D.play(obs_types[randi() % obs_types.size()])
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x += speed * delta
-	if position.x > get_viewport().size.x:
-		position.x = -$AnimatedSprite2D.texture.get_size().x
-
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
