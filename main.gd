530992b2a895156f26f5cb6bd5e9f14df132e9c8
Silver Rosario <147434626+SilverDontAsk@users.noreply.github.com>
2024-10-29 13:51:41 -0400

    Removing all the things that I made and storing them outside

--- main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	load_title_screen()
-	
-func load_title_screen() -> void:
-	var title_scene = ResourceLoader.load("res://Titlescreen/TitleScreen.tscn")
-	var title_instance = title_scene.instantiate()
-	get_tree().root.call_deferred("add_child", title_instance)
